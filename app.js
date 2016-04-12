b873f52848aceddc2c110af0e5ee94af67737e3a
Qiu Xiang <xiang.qiu@qq.com>
2016-04-12 15:37:21 +0800

    实现内存缓存

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var express = require('express')
-var app = express()
+var app = require('express')()
+var caches = {}
 
 app.get('/', function (req, res) {
-  var DataSource = require('./source/' + req.query.source)
-  var source = new DataSource()
-  source.get(req.query.page || 1).then(function (data) {
+  var cache = caches[req.url]
+  
+  if (cache && Date.now() - cache.time < 3600000) {
+    response(cache.data)
+  } else {
+    var source = new (require('./source/' + req.query.source || '500px'))()
+    source.get(req.query.page || 1).then(function (data) {
+      caches[req.url] = {
+        time: Date.now(),
+        data: data
+      }
+      response(data)
+    })
+  }
+  
+  function response(data) {
     res.set('Access-Control-Allow-Origin', '*')
     res.send(data)
-  })
+  }
 })
 
 app.listen(process.env.VCAP_APP_PORT || 3000)
