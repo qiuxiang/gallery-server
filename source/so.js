981e7287dba50c21cbda651c253fe06f0f5a7cc3
Qiu Xiang <xiang.qiu@qq.com>
2016-04-20 19:16:37 +0800

    修改接口

--- source/so.js
+++ source/so.js
@@ -17,7 +17,7 @@ DataSource.prototype.fieldsMap = function (item) {
     description: '标签：' + item.tag,
     width: parseInt(item.cover_width),
     height: parseInt(item.cover_height),
-    image: {
+    url: {
       small: item.cover_imgurl,
       large: item.cover_imgurl
     }
